0041404af8156b982d15c262c2190827a7e2679e
Maajid Husain <mah2ksc@virginia.edu>
2025-12-31 23:47:10 -0600

    update gh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import Unocss from '@unocss/vite'
 
 export default defineConfig({
-  base: '/halalhomestead/', // Add this line!
+  base: '/halalhomestead.github.io/', // Add this line!
   plugins: [
     react(),
     Unocss(),
